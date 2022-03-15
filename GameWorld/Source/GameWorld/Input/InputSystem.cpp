2f8cfea0886ea9ea6859b14fc1b2cada55f89715
Mocki <34432001+Morcki@users.noreply.github.com>
2022-03-15 10:48:30 +0800

    add input system

---
+++ GameWorld/Source/GameWorld/Input/InputSystem.cpp
@@ -0,0 +1,41 @@
+#include <PrecompiledHeader.h>
+#include "InputSystem.h"
+
+#include "../Application.h"
+#include <GLFW/glfw3.h>
+
+namespace GameWorld
+{
+	bool InputSystem::IsKeyPressed(const KeyCode key)
+	{
+		auto* window = static_cast<GLFWwindow*>(Application::GetInst().GetWindow().GetNativeWindow());
+		auto state = glfwGetKey(window, static_cast<int32_t>(key));
+		return state == GLFW_PRESS || state == GLFW_REPEAT;
+	}
+
+	bool InputSystem::IsMouseButtonPressed(const MouseCode button)
+	{
+		auto* window = static_cast<GLFWwindow*>(Application::GetInst().GetWindow().GetNativeWindow());
+		auto state = glfwGetMouseButton(window, static_cast<int32_t>(button));
+		return state == GLFW_PRESS;
+	}
+
+	glm::vec2 InputSystem::GetMousePosition()
+	{
+		auto* window = static_cast<GLFWwindow*>(Application::GetInst().GetWindow().GetNativeWindow());
+		double xpos, ypos;
+		glfwGetCursorPos(window, &xpos, &ypos);
+
+		return { (float)xpos, (float)ypos };
+	}
+
+	float InputSystem::GetMouseX()
+	{
+		return GetMousePosition().x;
+	}
+
+	float InputSystem::GetMouseY()
+	{
+		return GetMousePosition().y;
+	}
+}
---
+++ GameWorld/Source/GameWorld/Input/InputSystem.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <glm/glm.hpp>
+
+#include "../Events/KeyCodes.h"
+#include "../Events/MouseCodes.h"
+
+namespace GameWorld
+{
+	class InputSystem
+	{
+	public:
+		static bool IsKeyPressed(KeyCode key);
+
+		static bool IsMouseButtonPressed(MouseCode button);
+		static glm::vec2 GetMousePosition();
+		static float GetMouseX();
+		static float GetMouseY();
+	};
+}
