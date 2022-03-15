3f43407b005b088a682a9f56ef5a2327f2823485
Mocki <34432001+Morcki@users.noreply.github.com>
2022-03-15 17:49:30 +0800

    update premake5 file

---
+++ SandBox/premake5.lua
@@ -0,0 +1,55 @@
+project "SandBox"
+	location "SandBox"
+	kind "ConsoleApp"
+	language "C++"
+	staticruntime "off"
+	
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+	
+	files
+	{
+		"Source/**.h",
+		"Source/**.cpp"
+	}
+	
+	includedirs
+	{
+		"%{wks.location}/GameWorld/ThirdParty/spdlog/include",
+		"%{wks.location}/GameWorld/Source",
+		"%{wks.location}/GameWorld/ThirdParty",
+		"%{IncludeDir.GLM}",
+	}
+	
+	links
+	{
+		"GameWorld"
+	}
+	
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+		
+		defines
+		{
+			"GAMEWORLD_PLATFORM_WINDOWS",
+			"GAMEWORLD_ENABLE_ASSERT",
+		}
+		
+	filter "configurations:Debug"
+		defines "GAMEWORLD_DEBUG"
+		buildoptions "/MDd"
+		symbols "On"
+		
+	filter "configurations:Release"
+		defines "GAMEWORLD_RELEASE"
+		runtime "Release"
+		buildoptions "/MD"
+		symbols "On"
+	
+	filter "configurations:Dist"
+		defines "GAMEWORLD_DIST"
+		runtime "Release"
+		buildoptions "/MD"
+		symbols "On"
